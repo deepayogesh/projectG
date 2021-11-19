cb76dbbfd6954d6f317d3ace989d7c4fe3d1dbfd
Thomas Papiernik <tom.papiernik@gmail.com>
2021-11-19 15:14:56 -0600

    Updated database creation sql file and workflow steps

--- g-mvs-CreateTables_Tom2-db.sql
+++ g-mvs-CreateTables_Tom2-db.sql
@@ -1,26 +1,18 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
-CREATE TABLE "kaggle_metadata" (
-    "id" INT   NOT NULL,
-    "genres" VARCHAR(200)   NOT NULL,
-    "imdb_id" VARCHAR(20)   NOT NULL,
-    "Title" VARCHAR(200)   NOT NULL,
-    CONSTRAINT "pk_kaggle_metadata" PRIMARY KEY (
-        "id"
-     )
+﻿
+CREATE TABLE "title_basics" (
+    CONSTRAINT "tconst" VARCHAR(10) NOT NULL PRIMARY KEY,
+    "titleType" VARCHAR(5) NOT NULL,
+    "primaryTitle" VARCHAR(400) NOT NULL,
+    "originalTitle" VARCHAR(400) NOT NULL,
+    "isAdult" INT(1) NOT NULL,
+    "startYear" INT(4) NOT NULL,
+    "runtimeMinutes" INT(10) NOT NULL,
+    "genres" VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE "ratings" (
-    "movie_id" INT   NOT NULL,
-    "avg_rating" INT   NOT NULL,
-    CONSTRAINT "pk_ratings" PRIMARY KEY (
-        "movie_id"
-     )
-);
+-- PICK UP HERE...
 
-CREATE TABLE "watched_list" (
+CREATE TABLE "US_title_ids_unique" (
     "imdb_id" VARCHAR(20)   NOT NULL,
     "genres" VARCHAR(200)   NOT NULL,
     "Title" VARCHAR(200)   NOT NULL,
@@ -29,6 +21,15 @@ CREATE TABLE "watched_list" (
         "imdb_id"
      )
 );
+
+CREATE TABLE "title_ratings" (
+    "movie_id" INT   NOT NULL,
+    "avg_rating" INT   NOT NULL,
+    CONSTRAINT "pk_ratings" PRIMARY KEY (
+        "movie_id"
+     )
+);
+
 
 ALTER TABLE "kaggle_metadata" ADD CONSTRAINT "fk_kaggle_metadata_id" FOREIGN KEY("id")
 REFERENCES "ratings" ("movie_id");
