f27c51a26143a353e50df0bfbbd18a46ec6328d0
deepayogesh <32173556+deepayogesh@users.noreply.github.com>
2021-11-22 20:42:43 -0600

    Add files via upload
    Create tables sql file

---
+++ g_mvs_create_table-V 1.3.sql
@@ -0,0 +1,39 @@
+﻿-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "g_mvs_title_basics" (
+    "tconst" varchar(10)   NOT NULL,
+    "titletype" varchar(5)   NOT NULL,
+    "primarytitle" varchar(250)   NOT NULL,
+    "originaltitle" varchar(250)   NOT NULL,
+    "isadult" int   NOT NULL,
+    "startyear" int   NOT NULL,
+    "runtimeminutes" int   NOT NULL,
+    "genres" varchar(50)   NOT NULL,
+	CONSTRAINT "pk_g_mvs_title_basics" PRIMARY KEY (
+        "tconst"
+     )
+);
+
+CREATE TABLE "g_mvs_us_title_ids" (
+    "tconst" varchar(10)   NOT NULL,
+	CONSTRAINT "pk_g_mvs_us_title_ids" PRIMARY KEY (
+        "tconst"
+     )
+);
+
+CREATE TABLE "g_mvs_title_ratings" (
+    "tconst" varchar(10)   NOT NULL,
+    "averagerating" float   NOT NULL,
+    "numvotes" int   NOT NULL,
+	CONSTRAINT "pk_g_mvs_title_ratings" PRIMARY KEY (
+        "tconst"
+     )
+);
+
+--ALTER TABLE "g_mvs_us_title_ids" ADD CONSTRAINT "fk_g_mvs_us_title_ids_tconst" FOREIGN KEY("tconst")
+--REFERENCES "g_mvs_title_basics" ("tconst");
+
+--ALTER TABLE "g_mvs_title_ratings" ADD CONSTRAINT "fk_g_mvs_title_ratings_tconst" FOREIGN KEY("tconst")
+--REFERENCES "g_mvs_title_basics" ("tconst");
+
